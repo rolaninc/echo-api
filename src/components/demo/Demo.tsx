a720b7f4724184d277dc1fffdc8fc6ac7c5d10d1
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-03 12:42:59 +0900

    feat: implement toast in demo

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -1,16 +1,27 @@
 import Form, { Input } from './Form'
+import { useToast } from '../@ui/Toast'
 import { client } from '../../lib/http'
 
 const Demo = () => {
+  const toast = useToast()
+
   const onSubmit = async (input: Input) => {
     try {
       const data = JSON.parse(input.body)
       const ret = await client.post('/api/echo', data)
       console.log(JSON.stringify(ret.headers, undefined, 2))
       console.log(JSON.stringify(ret.data, undefined, 2))
+      toast({
+        title: 'Echo 🚀',
+        status: 'success',
+      })
       return true
     } catch (e) {
-      console.error(e)
+      toast({
+        title: 'Echo 💣',
+        description: e.message,
+        status: 'error',
+      })
     }
     return false
   }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Toast from '../components/@ui/Toast'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <Toast>
+      <Component {...pageProps} />
+    </Toast>
+  )
 }
 
 export default App

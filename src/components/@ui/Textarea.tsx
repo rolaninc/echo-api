51d00472564fad8639318da4e75f278bfe3a5b8b
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-03 20:07:49 +0900

    feat(client): add text area supporting tab input

---
+++ src/components/@ui/Textarea.tsx
@@ -0,0 +1,38 @@
+import { DetailedHTMLProps, forwardRef, TextareaHTMLAttributes } from 'react'
+
+type TextareaProps = DetailedHTMLProps<
+  TextareaHTMLAttributes<HTMLTextAreaElement>,
+  HTMLTextAreaElement
+>
+
+type Props = {} & TextareaProps //Omit<TextareaProps, 'onKeyDown'>
+
+const Textarea = forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
+  // convert tab key to 2 spaces
+  const modified = {
+    ...props,
+    onKeyDown: (e) => {
+      if (e.key === 'Tab' && e.keyCode !== 229) {
+        e.preventDefault()
+        const target = e.target
+        const spaces = '  '
+        const { selectionStart, selectionEnd, value } = target
+        target.value =
+          value.substring(0, selectionStart) +
+          spaces +
+          value.substring(selectionEnd, value.length)
+        target.setSelectionRange(
+          selectionStart + spaces.length,
+          selectionStart + spaces.length
+        )
+      } else {
+        if (props.onKeyDown) {
+          props.onKeyDown(e)
+        }
+      }
+    },
+  }
+  return <textarea ref={ref} {...modified} />
+})
+
+export default Textarea

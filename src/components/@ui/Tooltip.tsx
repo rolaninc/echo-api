9f00e995c67a2852646dc1da4fdbd9f54e0aa175
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-03 21:39:34 +0900

    feat(client): update tooltip's porps

--- src/components/@ui/Tooltip.tsx
+++ src/components/@ui/Tooltip.tsx
@@ -1,23 +1,27 @@
 import { ReactNode } from 'react'
 
 type Props = {
-  text?: string
+  title?: string
+  description?: string
   className?: string
   children?: ReactNode
 }
 
 const Tooltip = (props: Props) => {
-  const { text, className, children } = props
+  const { title, description, className, children } = props
   return (
     <div className="relative group">
-      {text && (
+      {(title || description) && (
         <div
-          className={`absolute -top-8 left-1/2 -translate-x-1/2 whitespace-nowrap z-50
+          className={`absolute -top-12 left-1/2 -translate-x-1/2 whitespace-pre z-50
           opacity-0 group-hover:opacity-100 transition pointer-events-none delay-300
           bg-black rounded-sm px-2 py-1 text-white text-xs font-semibold
           ${className}`}
         >
-          {text}
+          {title && <p>{title}</p>}
+          {description && (
+            <p className="text-[10px] font-normal">{description}</p>
+          )}
         </div>
       )}
 

3f40fc769e4221b536b14d27d4296aa06203b7f3
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-11 13:11:16 +0900

    feat: add name generator

---
+++ src/utils/auto-name-gen.ts
@@ -0,0 +1,59 @@
+const _SEEDS = {
+  first: [
+    'mary',
+    'james',
+    'patricia',
+    'robert',
+    'jennifer',
+    'john',
+    'linda',
+    'michael',
+    'elizabeth',
+    'david',
+    'barbara',
+    'william',
+    'susan',
+    'richard',
+    'jessica',
+    'joseph',
+    'sarah',
+    'thomas',
+    'karen',
+    'charles',
+  ],
+  last: [
+    'smith',
+    'johnson',
+    'williams',
+    'brown',
+    'jones',
+    'garcia',
+    'miller',
+    'davis',
+    'rodriguez',
+    'martinez',
+    'hernandez',
+    'lopez',
+    'gonzalez',
+    'wilson',
+    'anderson',
+    'thomas',
+    'taylor',
+    'moor',
+    'jackson',
+    'martin',
+  ],
+}
+
+type Seed = keyof typeof _SEEDS
+
+export const generateName = (
+  seeds: Seed[] = ['first', 'last'],
+  sep: string = ' '
+): string =>
+  seeds
+    .map((s) => {
+      const seed = _SEEDS[s]
+      return seed[Math.floor(Math.random() * (seed.length - 1))]
+    })
+    .join(sep)

6376ada0da15f9962d425ac612aed73981030827
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-14 13:22:22 +0900

    refactor(api): change cache-control values

--- src/pages/images/[...param].tsx
+++ src/pages/images/[...param].tsx
@@ -22,9 +22,14 @@ export const getServerSideProps = async (
     const data = serviceResponse.data
     const type = serviceResponse.headers['content-type'] ?? 'images/unknown'
     res.setHeader('Content-Type', type)
+
+    // a year = 31536000
+    // a month(30 days) = 2592000
+    // a week = 604800
+    // a day = 86400
     res.setHeader(
       'Cache-Control',
-      'public, immutable, no-transform, s-maxage=31536000, max-age=31536000'
+      'public, immutable, no-transform, s-maxage=604800, max-age=604800'
     )
     res.end(data, 'binary')
     return {

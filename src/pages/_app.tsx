70cbea5155fd1f8b7ff6e97d834627fd9d989e3b
Tomohiko Ikeda <tom-e-kid@users.noreply.github.com>
2022-12-17 18:18:48 +0900

    chore(client): add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import Toast from '../components/@ui/Toast'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Toast>
-      <Component {...pageProps} />
-    </Toast>
+    <>
+      <Toast>
+        <Component {...pageProps} />
+      </Toast>
+      <Analytics />
+    </>
   )
 }
 
